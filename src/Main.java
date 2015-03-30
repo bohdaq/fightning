677276fa45a7de3785283a29792cb941efd75192
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-03-30 17:20:17 +0300

    idea integration issue fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Let the combat begin. ");
         Pers persOne = new Pers(10, 4, 1);
         Pers persTwo = new Pers(10, 3, 2);
@@ -12,16 +16,16 @@ public class Main {
 
         while(!endOfGame){
             System.out.println("Pers One Hits [0-4]:");
-            int persOneHits = Integer.parseInt(System.console().readLine());
+            int persOneHits = scanner.nextInt();
 
             System.out.println("Pers One Blocks [0-4]:");
-            int persOneBlocks = Integer.parseInt(System.console().readLine());
+            int persOneBlocks = scanner.nextInt();
 
             System.out.println("Pers Two Hits [0-4]:");
-            int persTwoHits = Integer.parseInt(System.console().readLine());
+            int persTwoHits = scanner.nextInt();
 
             System.out.println("Pers Two Blocks [0-4]:");
-            int persTwoBlocks = Integer.parseInt(System.console().readLine());
+            int persTwoBlocks = scanner.nextInt();
 
             endOfGame = fp.fight(persOneHits, persOneBlocks, persTwoHits, persTwoBlocks);
         }
